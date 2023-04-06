6d3a4d88dc6eaabfe13e5a93d5492273a0d76c09
CHANDAN KUMAR MALLICK <chandansoumya28@gmail.com>
2023-04-07 04:47:36 +0530

    Updated till Q17

---
+++ src/Assignment1/A1Q12.java
@@ -0,0 +1,24 @@
+/*
+Given an array arr[], find the maximum distance of index j and I, such
+that arr[j]<arr[I].
+ */
+package Assignment1;
+public class A1Q12 {
+    public static int findMaxDistance(int[] arr) {
+        int maxDistance = -1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] >= arr[i] && j - i > maxDistance) {
+                    maxDistance = j - i;
+                }
+            }
+        }
+        return maxDistance;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 4, 2, 6, 3, 7, 8, 1};
+        int maxDistance = findMaxDistance(arr);
+        System.out.println("Maximum distance is: " + maxDistance);
+    }
+}
